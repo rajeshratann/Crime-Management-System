aaed969b48bdd207367fde51511332b78a9e14a5
Rajesh Kumar <48724570+rajeshratann@users.noreply.github.com>
2021-07-31 09:38:24 +0530

    updated file
    update

---
+++ CrimeManagement/src/crimemanagement/authorization.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crimemanagement;
+
+/**
+ *
+ * @author Bhardwaj
+ */
+public class authorization {
+    public static String username="system",password="system";
+}
